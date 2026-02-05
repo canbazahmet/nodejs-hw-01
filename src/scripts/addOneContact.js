0cf01ceb829feb9590da6c9cccb29adb6d01abe0
Ahmet Canbaz <ahmet-canbaz@hotmail.com>
2026-02-05 23:39:58 +0300

    addOneContacts finish

--- src/scripts/addOneContact.js
+++ src/scripts/addOneContact.js
@@ -1,3 +1,15 @@
-export const addOneContact = async () => {};
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
+import { createFakeContact } from '../utils/createFakeContact.js';
 
-addOneContact();
+export const addOneContact = async () => {
+  const contacts = await readContacts();
+  const newContact = createFakeContact();
+  contacts.push(newContact);
+  await writeContacts(contacts);
+  console.log('Added 1 contact:', newContact.id);
+};
+
+if (process.argv[1]?.endsWith('addOneContact.js')) {
+  await addOneContact();
+}
