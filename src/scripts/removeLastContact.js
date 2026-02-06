9ee8e2c968459dcad4446a5e87d75fb814692183
Ahmet Canbaz <ahmet-canbaz@hotmail.com>
2026-02-06 14:23:33 +0300

    removeLastContact finish

--- src/scripts/removeLastContact.js
+++ src/scripts/removeLastContact.js
@@ -1,3 +1,17 @@
-export const removeLastContact = async () => {};
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
 
-removeLastContact();
+export const removeLastContact = async () => {
+  const contacts = await readContacts();
+  if (contacts.length === 0) {
+    console.log('No contacts to remove.');
+    return;
+  }
+  const removed = contacts.pop();
+  await writeContacts(contacts);
+  console.log('Removed last contact:', removed.id);
+};
+
+if (process.argv[1]?.endsWith('removeLastContact.js')) {
+  await removeLastContact();
+}
